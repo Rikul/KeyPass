a9cc9fa3cd21e87cb12eef197c25eccdd2264319
rikulpatel@gmail.com <rikulpatel@gmail.com>
2026-01-01 12:31:57 -0600

    updated sdk version to 35

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -46,7 +46,7 @@ kotlin {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
         minSdk = 21
